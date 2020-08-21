21d057f0ee9accfd910c4836aa6ea77fb2fea05c
oleoot <oleoot28@gmail.com>
2020-08-21 12:25:27 +0300

    Displaying messages from database to the page

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -4,10 +4,12 @@ import { useParams } from 'react-router-dom'
 import StarBorderIcon from '@material-ui/icons/StarBorder';
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 import db from '../../firebase'
+import Message from '../Message/Message'
 
 function Chat() {
     const { roomId } = useParams();
     const [roomDetails, setRoomDetails] = useState(null)
+    const [roomMessages, setRoomMessages] = useState([])
 
     useEffect(() => {
         if (roomId) {
@@ -15,6 +17,9 @@ function Chat() {
                 setRoomDetails(snapshot.data())
             ))
         }
+        db.collection('rooms').doc(roomId).collection('messages').orderBy('timestamp', 'asc').onSnapshot(snapshot =>
+            setRoomMessages(snapshot.docs.map((doc) => doc.data()))
+        )
     }, [roomId])
 
     return (
@@ -31,6 +36,12 @@ function Chat() {
                         <InfoOutlinedIcon />Details
                         </p>
                 </div>
+            </div>
+
+            <div className="chat__messages">
+                {roomMessages.map(({ message, timestamp, user, userImage }) => (
+                    <Message message={message} timestamp={timestamp} user={user} userImage={userImage} />
+                ))}
             </div>
         </div>
     )
---
+++ src/components/Message/Message.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Message.css'
+
+function Message({ message, timestamp, user, userImage }) {
+    return (
+        <div className="message">
+            <img src={userImage} alt='' />
+            <div className="message__info">
+                <h4>
+                    {user} timestamp...
+    <p>{message}</p>
+                </h4>
+            </div>
+        </div>
+    )
+}
+
+export default Message
