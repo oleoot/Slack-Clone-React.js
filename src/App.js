e783211213676b1c58ba7c970c946931c97e15a4
oleoot <oleoot28@gmail.com>
2020-08-21 11:22:26 +0300

    React router added + chat header added

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header'
 import Sidebar from './components/Sidebar/Sidebar'
+import Chat from './components/Chat/Chat'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    // BEM naming convention
     <div className="app">
-      <Header />
-      <div className="app__body">
-        <Sidebar />
-        {/* Sidebar */}
-        {/* React Router -> chat screen*/}
-      </div>
-
+      <Router>
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+          <Switch>
+            <Route path="/room/:roomId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <h1>Welcome</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
